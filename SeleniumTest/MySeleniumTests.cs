474292cc4574f766540a4b023d176aab6b271d83
ahmeds <ahmeds@ms.com>
2019-02-25 14:40:22 +0200

    hardcode chrome

--- SeleniumTest/MySeleniumTests.cs
+++ SeleniumTest/MySeleniumTests.cs
@@ -60,7 +60,7 @@ namespace SeleniumTest
             string chromepath= Environment.GetEnvironmentVariable("ChromeWebDriver");
             string chromeexe = "chromedriver.exe";
             string fullchromeexe = chromepath + chromeexe;
-
+            fullchromeexe = @"C:\SeleniumWebDrivers\ChromeDriver\chromedriver.exe";
             string browser = "Chrome";
             switch (browser)
             {
